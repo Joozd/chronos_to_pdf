8b9b423c96aba4a498ce728c7ce7dd56f874d784
joozd <euroshopper@gmail.com>
2023-08-27 18:43:20 +0200

    Generate new user

--- src/main/kotlin/data/LoginDataRepository.kt
+++ src/main/kotlin/data/LoginDataRepository.kt
@@ -2,13 +2,16 @@ package data
 
 import SecureHasher
 import data.logindata.LoginData
+import data.logindata.LoginWithKey
 import data.logindata.Users
+import data.security.Encryption
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.util.*
 
 object LoginDataRepository {
-    fun insertUser(user: LoginData) {
+    private fun insertUser(user: LoginData) {
         transaction {
             Users.insert {
                 it[username] = user.username
@@ -36,5 +39,33 @@ object LoginDataRepository {
         val user = getUserByUsername(uid) ?: return false
         // check if the stored hash is the same as calculated hash
         return SecureHasher.hashKeyWithSalt(base64key, uid, user.salt).contentEquals(user.hash)
+    }
+
+    /**
+     * Check if a username already exists
+     */
+    private fun checkIfUserExists(uid: String): Boolean = getUserByUsername(uid) != null
+
+    /**
+     * Generate a username+password combination and stores its salt+hash.
+     * TODO: Check if user doesn't exist yet (edge case)
+     */
+    fun generateUser(): LoginWithKey{
+        // Generate a unique username
+        var userName: String
+        do
+            userName = Encryption.generateUserName()
+        while(checkIfUserExists(userName))
+
+        val key = Encryption.generateSecureRandomData()
+        val salt = Encryption.generateSecureRandomData(16)
+
+        val hash = SecureHasher.hashKeyWithSalt(key, userName, salt)
+
+        val base64key = Base64.getEncoder().encodeToString(key)
+
+        insertUser(LoginData(userName, salt, hash))
+
+        return LoginWithKey(userName, base64key)
     }
 }
