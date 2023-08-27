3840a7d2126ff8668a62c28fab4aeb8d61411ec7
joozd <euroshopper@gmail.com>
2023-08-27 18:42:12 +0200

    Generated equals and hashCode functions (Arrays in data class constructor)

--- src/main/kotlin/data/logindata/LoginData.kt
+++ src/main/kotlin/data/logindata/LoginData.kt
@@ -1,3 +1,26 @@
 package data.logindata
 
-data class LoginData(val username: String, val salt: ByteArray, val hash: ByteArray)
+import data.security.Encryption
+import java.security.SecureRandom
+
+data class LoginData(val username: String, val salt: ByteArray, val hash: ByteArray){
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as LoginData
+
+        if (username != other.username) return false
+        if (!salt.contentEquals(other.salt)) return false
+        if (!hash.contentEquals(other.hash)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = username.hashCode()
+        result = 31 * result + salt.contentHashCode()
+        result = 31 * result + hash.contentHashCode()
+        return result
+    }
+}
