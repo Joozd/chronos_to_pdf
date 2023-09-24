f4c1462ce176c910cc6390d729ac6a4f6bdc639f
joozd <euroshopper@gmail.com>
2023-09-24 15:39:30 +0200

    Moved sim aircraft type guessing to GeneralPostProcessor

--- src/main/kotlin/postprocessing/AircraftPostprocessor.kt
+++ src/main/kotlin/postprocessing/AircraftPostprocessor.kt
@@ -3,7 +3,6 @@ package postprocessing
 import data.PreferencesData
 import data.R
 import nl.joozd.joozdlogcommon.BasicFlight
-import java.util.*
 
 /**
  * This takes care of aircraft types and if they are multi pilot or not.
@@ -27,40 +26,9 @@ object AircraftPostprocessor: PostProcessor() {
             isCoPilot = isCopilot,
             multiPilotTime = multiPilotTime,
             )
-    }.postProcessSim(preferencesData)
-
-    /**
-     * This guesses the type of sim based on what aircraft has been flown on the first flight after this simulator duty.
-     * Does not change type if it has been entered.
-     * THIS NEEDS A SORTED LIST
-     */
-    private fun List<BasicFlight>.postProcessSim(preferencesData: PreferencesData): List<BasicFlight> { // = mapIndexed{ i, f ->
-        if (!preferencesData.addTypeToSim) return this
-
-        val currentList = LinkedList(this)
-
-        var currentTime = Long.MIN_VALUE
-
-        return buildList {
-            while (currentList.isNotEmpty()) {
-                val f = currentList.removeFirst()
-
-                // If list is unsorted, this will fail.
-                if (f.timeOut < currentTime){
-                    logger.error("Trying to get aircraft type with an unsorted list")
-                    return this
-                }
-                currentTime = f.timeOut
-
-                if (f.isSim && f.aircraft.isBlank()) {
-                    val type = currentList.firstOrNull { it.aircraft.isNotBlank() }?.aircraft ?: ""
-                    add(f.copy(aircraft = type))
-                }
-                else
-                    add(f)
-            }
-        }.toList()
     }
+
+
 
     private fun buildAircraftMap(): Map<String, Aircraft>{
         val aircraftList: List<Aircraft> = R.fromJson("aircraft.json")
--- src/main/kotlin/postprocessing/GeneralPostProcessor.kt
+++ src/main/kotlin/postprocessing/GeneralPostProcessor.kt
@@ -3,6 +3,7 @@ package postprocessing
 import data.PreferencesData
 import nl.joozd.joozdlogcommon.BasicFlight
 import nl.joozd.pdflogbookbuilder.extensions.totalTime
+import java.util.*
 
 /**
  * This takes care of postprocessing items that are not tied to aircraft or airport data.
@@ -16,5 +17,39 @@ object GeneralPostProcessor: PostProcessor() {
             ifrTime = f.totalTime(),
             name = name
         ) // all imported flights are IFR.
+    }.postProcessSim(preferencesData)
+
+
+    /**
+     * This guesses the type of sim based on what aircraft has been flown on the first flight after this simulator duty.
+     * Does not change type if it has been entered.
+     * THIS NEEDS A SORTED LIST
+     */
+    private fun List<BasicFlight>.postProcessSim(preferencesData: PreferencesData): List<BasicFlight> { // = mapIndexed{ i, f ->
+        if (!preferencesData.addTypeToSim) return this
+
+        val currentList = LinkedList(this)
+
+        var currentTime = Long.MIN_VALUE
+
+        return buildList {
+            while (currentList.isNotEmpty()) {
+                val f = currentList.removeFirst()
+
+                // If list is unsorted, this will fail.
+                if (f.timeOut < currentTime){
+                    logger.error("Trying to get aircraft type with an unsorted list")
+                    return this
+                }
+                currentTime = f.timeOut
+
+                if (f.isSim && f.aircraft.isBlank()) {
+                    val type = currentList.firstOrNull { it.aircraft.isNotBlank() }?.aircraft ?: ""
+                    add(f.copy(aircraft = type))
+                }
+                else
+                    add(f)
+            }
+        }.toList()
     }
 }
