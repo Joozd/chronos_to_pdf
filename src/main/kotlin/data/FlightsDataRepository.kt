9b0740f97b29be738ecaeaf8c68a10153888b838
joozd <euroshopper@gmail.com>
2023-09-25 16:19:22 +0200

    Preprocessing for imported flights

--- src/main/kotlin/data/FlightsDataRepository.kt
+++ src/main/kotlin/data/FlightsDataRepository.kt
@@ -3,8 +3,10 @@ package data
 import data.flightsdata.EncryptedUserData
 import data.logindata.LoginWithKey
 import data.security.Encryption
+import nl.joozd.joozdlogcommon.AugmentedCrew
 import nl.joozd.joozdlogcommon.BasicFlight
 import nl.joozd.joozdlogimporter.interfaces.FileImporter
+import nl.joozd.joozdlogimporter.supportedFileTypes.ImportedFile
 import nl.joozd.joozdlogimporter.supportedFileTypes.PlannedFlightsFile
 import org.jetbrains.exposed.sql.statements.api.ExposedBlob
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -60,13 +62,14 @@ object FlightsDataRepository {
         createOrUpdateEncryptedDataForUser(username, encryptedCSV)
     }
 
+    //TODO make this accessible only from [Session]
     fun insertDataForUser(loginWithKey: LoginWithKey, flights: Collection<BasicFlight>) =
         insertDataForUser(loginWithKey.uid, loginWithKey.base64Key, flights)
 
     /**
      * Gets data for user. Note that FlightID is not used and will always be -1.
      */
-    fun getDataForUser(username: String, base64Key: String): Collection<BasicFlight>? {
+    private fun getDataForUser(username: String, base64Key: String): Collection<BasicFlight>? {
         val encryptedUserData = getEncryptedDataForUser(username) ?: return null
         val decryptedData = Encryption.decryptDataToString(encryptedUserData, base64Key) ?: return null
 
@@ -74,15 +77,20 @@ object FlightsDataRepository {
         return decryptedData.lines().filter { it.isNotBlank() }.map { BasicFlight.ofCsv(it) }
     }
 
+    //TODO make this accessible only from [Session] (breaks tests at this time)
     fun getDataForUser(loginWithKey: LoginWithKey) =
         getDataForUser(loginWithKey.uid, loginWithKey.base64Key)
 
     /**
      * Create new entry for user. Removes old data for that user.
      */
+    //TODO make this accessible only from [Session]
     fun createNewDataForUser(loginWithKey: LoginWithKey) = insertDataForUser(loginWithKey, emptyList())
 
 
+    /**
+     * A session holds login data for a user, so it can access the user's data.
+     */
     class Session(username: String, base64Key: String){
         private val loginWithKey = LoginWithKey(username, base64Key)
 
@@ -91,7 +99,7 @@ object FlightsDataRepository {
          */
         fun addFlightsFromFiles(importers: Collection<FileImporter>, preferencesData: PreferencesData): List<BasicFlight>{
             val knownFlights = getDataForUser(loginWithKey) ?: return emptyList() // maybe throw an error about not being logged in?
-            val flights = importers.mapNotNull { it.getFile().takeIf { file -> file !is PlannedFlightsFile }?.getFlights() ?: emptyList() }
+            val flights = importers.map { it.getFile().extractFlights(preferencesData) }
                 .flatten()
 
             val result = (flights + knownFlights) // new flights go  first, so they are saved (in case my algorithm gets improved)
@@ -102,6 +110,28 @@ object FlightsDataRepository {
             insertDataForUser(loginWithKey, result)
 
             return result
+        }
+
+        /**
+         * Extracts flights.
+         * Uses [preferencesData] for preprocessing
+         *  (marking flights as PIC for extractors that do not support detection of rank)
+         */
+        private fun ImportedFile.extractFlights(preferencesData: PreferencesData): List<BasicFlight>{
+            if(this is PlannedFlightsFile) return emptyList() // rosters are not supported, only monthlies and complete logbooks
+            val flights = getFlights() ?: emptyList()
+            return flights.map{f ->
+                //isPic is, if supported, what the parser gave us; else what preferencesData gives us.
+                val isPic = if(supports(ImportedFile.RANK)) f.isPIC else preferencesData.function == PreferencesData.CAPTAIN
+
+                // augmented crew is, if supported, what the parser gave us, else its 3 crew if user is SO or 2 crew is user is Captain of FO (prom preferencesData)
+                val crew: Int = if(supports(ImportedFile.RANK)) f.augmentedCrew
+                else
+                    if (preferencesData.function == PreferencesData.SO) AugmentedCrew.coco(takeoffLandingTimes = 0).toInt()
+                    else 0
+
+                f.copy(isPIC = isPic, augmentedCrew = crew)
+            }
         }
     }
 }
