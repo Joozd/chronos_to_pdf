8d5633ff9767ac73b42688c31ab75062e93b8743
joozd <euroshopper@gmail.com>
2023-09-26 11:35:44 +0200

    toJson

--- src/main/kotlin/data/PreferencesData.kt
+++ src/main/kotlin/data/PreferencesData.kt
@@ -2,6 +2,7 @@ package data
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 /**
  * Preferences for postprocessing flights
@@ -12,8 +13,9 @@ data class PreferencesData @JsonCreator constructor(
     @JsonProperty("removeSimTypes") val removeSimTypes: Boolean,
     @JsonProperty("function") val function: String,
 ){
+    fun toJson() = jacksonObjectMapper().writeValueAsString(this)
     companion object{
-        val DEFAULT get() = PreferencesData(logLanding = true, guessSimType = true, function = FO, removeSimTypes = false)
+        val DEFAULT get() = PreferencesData(logLanding = true, guessSimType = true, removeSimTypes = false, function = FO)
         // Function names
         const val CAPTAIN = "Captain"
         const val FO = "FO"
